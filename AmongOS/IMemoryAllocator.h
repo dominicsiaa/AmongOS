e4481b48197dbdef3b8d8764e7414b9284a0c232
Dane Chan <dane_chan@dlsu.edu.ph>
2024-11-08 02:33:41 +0800

    fix: changed return type of allocate() to bool

--- AmongOS/IMemoryAllocator.h
+++ AmongOS/IMemoryAllocator.h
@@ -7,7 +7,7 @@ public:
 	IMemoryAllocator() = default;
 	virtual ~IMemoryAllocator() = default;
 
-	virtual void* allocate(size_t size, int pid) = 0;
+	virtual bool allocate(size_t size, int pid) = 0;
 	virtual void deallocate(int pid) = 0;
 	virtual String visualizeProcessesInMemory() = 0;
 };
